f57e800aa303be3b4ae4e3bd147d1cec12fb217c
Chris Lenz <chris@freelenzer.at>
2021-11-10 10:06:04 +0100

    Fix client cache
    * updated dependencies
    
    * remove client cache
    
    Co-authored-by: Chris Lenz <chris@rtbrick.com>

--- pkg/rbfs/commons/client.go
+++ pkg/rbfs/commons/client.go
@@ -8,36 +8,16 @@ package commons
 import (
 	"net/http"
 	"net/url"
-	"sync"
 
 	"github.com/rtbrick/go-rbfs-client/pkg/rbfs/state"
 )
 
-var clients = make(map[string]*state.APIClient)
-var x = &sync.Mutex{}
-
 // GetAPIClient creates a new API client for the given endpoint.
 func GetAPIClient(client *http.Client, endpoint *url.URL) *state.APIClient {
-	host := endpoint.Host
-
-	apiClient := clients[host]
-
-	if apiClient == nil {
-		x.Lock()
-		defer x.Unlock()
-		apiClient = clients[host]
-		if apiClient == nil {
-			// If client is still nil, i.e. was not created in the meanwhile by another request,
-			// then create a new client.
-			config := state.NewConfiguration()
-			config.BasePath = endpoint.String()
-			config.Host = endpoint.Host
-			config.HTTPClient = client
-			config.UserAgent = "Diagnostic Actor"
-			apiClient = state.NewAPIClient(config)
-			clients[host] = apiClient
-		}
-	}
-
-	return apiClient
+	config := state.NewConfiguration()
+	config.BasePath = endpoint.String()
+	config.Host = endpoint.Host
+	config.HTTPClient = client
+	config.UserAgent = "Diagnostic Actor"
+	return state.NewAPIClient(config)
 }
